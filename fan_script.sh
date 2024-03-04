e5d6fe334afde1feffffcb4c4549bbf9f0e494a0
Sam St-Pettersen <s.stpettersen@pm.me>
2024-03-04 16:03:06 +0000

    modify script with loop and 5 second sleep.

--- fan_script.sh
+++ fan_script.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-temp=$(/usr/bin/vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+while true; do
 
-if (( $(echo "$temp < 55" | bc -l) )); then
-    temp=0
-elif (( $(echo "$temp < 60" | bc -l) )); then
-    temp=1
-elif (( $(echo "$temp < 68" | bc -l) )); then
-    temp=2
-elif (( $(echo "$temp < 75" | bc -l) )); then
-    temp=3
-else
-    temp=4
-fi
+	temp=$(/usr/bin/vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
 
-echo $temp > /sys/class/thermal/cooling_device0/cur_state
+	if (( $(echo "$temp < 55" | bc -l) )); then
+    		temp=0
+	elif (( $(echo "$temp < 60" | bc -l) )); then
+    		temp=1
+	elif (( $(echo "$temp < 68" | bc -l) )); then
+    		temp=2
+	elif (( $(echo "$temp < 75" | bc -l) )); then
+    		temp=3
+	else
+    		temp=4
+	fi
+
+	echo $temp > /sys/class/thermal/cooling_device0/cur_state
+	sleep 5;
+done
